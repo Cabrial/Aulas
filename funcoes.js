6ddc2b22f5b0d7d79662febd002c6347bf8b198a
Pedro Cabrial <pedrocabrial@gmail.com>
2021-03-13 20:20:59 -0300

    Aula Funções e JSON

---
+++ funcoes.js
@@ -0,0 +1,20 @@
+function multiplicacao (p1, p2){
+    console.log("Function multiplicacao executada!");
+    return p1*p2;
+}
+
+function saudacao(nome){
+    console.log("Olá " + nome + ", seja bem vindo!");
+}
+
+function funcaoSemParametro (){
+    var a = 1;
+    var b = 2;
+    console.log(a + b);
+}
+
+//var resultado = multiplicacao (4, 3);
+//console.log(resultado)
+//console.log(multiplicacao(10, 3));
+saudacao("Pedro");
+//funcaoSemParametro();
